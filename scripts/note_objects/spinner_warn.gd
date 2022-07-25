758e9d4a2cc4c7c525a397272ba803747702f934
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-24 21:01:37 -0700

    Refactor `SpinnerWarn`

--- scripts/note_objects/spinner_warn.gd
+++ scripts/note_objects/spinner_warn.gd
@@ -1,43 +1,55 @@
+class_name SpinnerWarn
 extends KinematicBody2D
 
-onready var spinnerObj = preload("res://game/objects/spinner_object.tscn")
+var timing := 0.0
 
-export var timing = 0
-export var speed = 1
-export var length = 1
+var _active := false
+var _bpm := 1.0
+var _length := 1.0
+var _speed := 1.0
 
-export var active = false
+onready var _gameplay := $"../../../../.." as Gameplay
+onready var _spinner_obj := preload("res://game/objects/spinner_object.tscn")
 
-var vel: Vector2
 
-func _process(_delta) -> void:
+func _process(_delta: float) -> void:
 	# move note if not hit yet
-	if(active == true): 
-		vel = move_and_slide(Vector2((speed * -1.9), 0))
-		if (get_node("../../../../../Music").get_playback_position() >= timing):
+	if _active:
+		var _vel := move_and_slide(Vector2(_speed * -1.9, 0))
+		if _gameplay.music.get_playback_position() >= timing:
 			deactivate()
 
-func changeProperties(newTiming, newSpeed, newLength):
-	timing = newTiming
-	speed = newSpeed
-	length = newLength
+
+func change_properties(new_timing: float, new_speed: float, new_length: float, new_bpm: float) -> void:
+	if _active:
+		push_warning("Attempted to change spinner warning properties after active.")
+		return
+	timing = new_timing
+	_bpm = new_bpm
+	_length = new_length
+	_speed = new_speed
+
 
 func activate() -> void:
-	modulate = Color(1,1,1,1)
-	position = Vector2(timing * speed, 0)
-	active = true
+	if _active:
+		push_warning("Attempted to activate spinner warning after active.")
+		return
+	modulate = Color.white
+	position = Vector2(_speed * timing, 0)
+	_active = true
+
 
 func deactivate() -> void:
-	#make spinner obj first
-	var spinner = spinnerObj.instance()
-	#note to self; couldnt i just give the cur sv timing from the chart loader? seems redundant to do this and
-	#possibly more laggy honestly
-	var chartSV = get_node("../../../../../ChartLoader").findTiming(timing)
-	var hitsRequired = floor(length / ((chartSV[0] / 60)) * 16)
-	spinner.change_properties(timing, length, hitsRequired)
+	if not _active:
+		push_warning("Attempted to deactivate spinner warning before active.")
+		return
+
+	# make spinner obj first
+	var spinner := _spinner_obj.instance() as Spinner
+	spinner.change_properties(timing, _length, int(_length * 960 / _bpm))
 	get_parent().add_child(spinner)
 	get_parent().move_child(spinner, 0)
-	
-	#make self deactive (duh!)
-	modulate = Color(0,0,0,0)
-	active = false
+
+	# make self deactive (duh!)
+	modulate = Color.transparent
+	_active = false
