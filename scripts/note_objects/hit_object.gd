4dd8913f04ae5f160f6c95414c55f6cbbf01390e
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-24 21:56:48 -0700

    Add `HitObject` and implement

---
+++ scripts/note_objects/hit_object.gd
@@ -0,0 +1,51 @@
+class_name HitObject
+extends KinematicBody2D
+
+var finisher := false
+var timing := 0.0
+
+var _active := false
+var _length := 1.0
+var _loaded := false
+var _speed := 1.0
+
+onready var _gameplay := $"../../../../.." as Gameplay
+
+
+func _ready() -> void:
+	# finisher scale
+	if finisher:
+		(get_child(0) as Control).rect_scale = Vector2(0.9, 0.9)
+	_loaded = true
+
+
+func _process(_delta: float) -> void:
+	# move note if not hit yet
+	if _active:
+		var _vel := move_and_slide(Vector2(_speed * -1.9, 0))
+
+
+func activate() -> void:
+	if _active or not _loaded:
+		push_warning("Attempted to activate hitobject.")
+		return
+	modulate = Color.white
+	position = Vector2(_speed * timing, 0)
+	_active = true
+
+
+func deactivate() -> void:
+	if not _active or not _loaded:
+		push_warning("Attempted to deactivate hitobject.")
+	modulate = Color.transparent
+	_active = false
+
+
+func ini(new_timing: float, new_speed: float, new_length: float, new_finisher := false) -> void:
+	if _loaded:
+		push_warning("Attempted to change hitobject properties after loaded.")
+		return
+	finisher = new_finisher
+	timing = new_timing
+	_length = new_length
+	_speed = new_speed
--- scripts/note_objects/spinner_warn.gd
+++ scripts/note_objects/spinner_warn.gd
@@ -1,49 +1,23 @@
 class_name SpinnerWarn
-extends KinematicBody2D
+extends HitObject
 
-var timing := 0.0
-
-var _active := false
 var _bpm := 1.0
-var _length := 1.0
-var _speed := 1.0
 
-onready var _gameplay := $"../../../../.." as Gameplay
 onready var _spinner_obj := preload("res://game/objects/spinner_object.tscn")
 
 
 func _process(_delta: float) -> void:
-	# move note if not hit yet
-	if _active:
-		var _vel := move_and_slide(Vector2(_speed * -1.9, 0))
-		if _gameplay.music.get_playback_position() >= timing:
-			deactivate()
+	if _active and _gameplay.music.get_playback_position() >= timing:
+		deactivate()
 
 
 func change_properties(new_timing: float, new_speed: float, new_length: float, new_bpm: float) -> void:
-	if _active:
-		push_warning("Attempted to change spinner warning properties after active.")
-		return
-	timing = new_timing
-	_bpm = new_bpm
-	_length = new_length
-	_speed = new_speed
-
-
-func activate() -> void:
-	if _active:
-		push_warning("Attempted to activate spinner warning after active.")
-		return
-	modulate = Color.white
-	position = Vector2(_speed * timing, 0)
-	_active = true
+	.ini(new_timing, new_speed, new_length)
+	if not _loaded:
+		_bpm = new_bpm
 
 
 func deactivate() -> void:
-	if not _active:
-		push_warning("Attempted to deactivate spinner warning before active.")
-		return
-
 	# make spinner obj first
 	var spinner := _spinner_obj.instance() as Spinner
 	spinner.change_properties(timing, _length, int(_length * 960 / _bpm))
@@ -51,5 +25,4 @@ func deactivate() -> void:
 	get_parent().move_child(spinner, 0)
 
 	# make self deactive (duh!)
-	modulate = Color.transparent
-	_active = false
+	.deactivate()
