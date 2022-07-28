56762b0b625f2d024e4b341c2d07322e70527399
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-28 14:59:11 -0700

    Integrate `offset_thingy` into `settings_panel`

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -3,7 +3,11 @@ extends Node
 
 var skin := SkinManager.new()
 
-onready var _judgements := $"BarRight/HitPointOffset/Judgements" as Node
+onready var _hit_point_offset := $"BarRight/HitPointOffset"
+
+onready var _object_container := _hit_point_offset.get_node("ObjectContainers") as Control
+
+onready var _judgements := _hit_point_offset.get_node("Judgements")
 
 onready var accurate_obj := _judgements.get_node("JudgeAccurate") as CanvasItem
 onready var inaccurate_obj := _judgements.get_node("JudgeInaccurate") as CanvasItem
@@ -33,3 +37,9 @@ func hit_error_toggled(new_visible: bool) -> void:
 func late_early_changed(new_value: int) -> void:
 	_hit_error.lateearlySimpleDisplay = new_value < 2
 	_timing_indicator.visible = new_value > 0
+
+
+func offset_changed() -> void:
+	# this is fundamentally flawed due to everything being scaled by 1.9
+	# it's a close approximation but should be fixed once scaling is removed
+	_object_container.rect_position = Vector2(settings.globalOffset * -775, 0)
--- scripts/settings_panel.gd
+++ scripts/settings_panel.gd
@@ -2,13 +2,27 @@ extends CanvasItem
 
 signal hit_error_toggled(new_visible)
 signal late_early_changed(new_value)
+signal offset_changed
+
+onready var _v := $"ScrollContainer/VBoxContainer"
 
 
 func _ready() -> void:
-	var late_early_drop := $"ScrollContainer/VBoxContainer/ExtraDisplays/LateEarly/OptionButton" as OptionButton
+	var late_early_drop := _v.get_node("ExtraDisplays/LateEarly/OptionButton") as OptionButton
 	late_early_drop.add_item("Off")
 	late_early_drop.add_item("Simple")
 	late_early_drop.add_item("Advanced")
+
+	var offset_text := _v.get_node("Audio/Offset/LineEdit") as LineEdit
+	if settings.globalOffset != 0:
+		offset_text.text = str(settings.globalOffset)
+
+
+# this script stinks. gonna become obsolete when i get actual good settings going w
+func change_offset(new_value: String) -> void:
+	settings.globalOffset = float(new_value) / 1000
+	print_debug("Offset set to: %s" % settings.globalOffset)
+	emit_signal("offset_changed")
 
 
 func hit_error(new_visible: bool) -> void:
