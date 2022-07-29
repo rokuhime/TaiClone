34cac71cc2290e704b3ca0e97548a02655e95756
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-28 20:42:40 -0700

    Integrate `KeybindChanger` into `SettingsPanel`

--- scripts/settings_panel.gd
+++ scripts/settings_panel.gd
@@ -5,15 +5,24 @@ signal hit_error_toggled(new_visible)
 signal late_early_changed(new_value)
 signal offset_changed
 
+const KEYBINDS := {}
+
 var global_offset := 0.0
 
 var _config_path := "user://config.ini"
+var _currently_changing := ""
 
 onready var _root := $"/root" as Viewport
 onready var _v := $"ScrollContainer/VBoxContainer"
 
 onready var _dropdown := _v.get_node("Resolution/OptionButton") as OptionButton
-onready var _keybind_changer := _v.get_node("Keybinds") as KeybindChanger
+onready var _keybinds := _v.get_node("Keybinds")
+
+onready var _left_don_butt := _keybinds.get_node("LeftDon/Button") as Button
+onready var _left_kat_butt := _keybinds.get_node("LeftKat/Button") as Button
+onready var _right_don_butt := _keybinds.get_node("RightDon/Button") as Button
+onready var _right_kat_butt := _keybinds.get_node("RightKat/Button") as Button
+
 
 
 func _ready() -> void:
@@ -23,8 +32,12 @@ func _ready() -> void:
 		print_debug("Config file not found.")
 		save_settings()
 		return
+
 	for key in config_file.get_section_keys("Keybinds"):
-		_keybind_changer.change_key(str(key), config_file.get_value("Keybinds", str(key)))
+		change_key(str(key), config_file.get_value("Keybinds", str(key)))
+	for button in ["LeftDon", "LeftKat", "RightDon", "RightKat"]:
+		var action_list := InputMap.get_action_list(str(button))
+		change_text(str(button), action_list[0])
 
 	var sections := config_file.get_sections()
 
@@ -59,6 +72,35 @@ func _ready() -> void:
 	_dropdown.set_item_metadata(7, Vector2(1025, 820))
 
 
+func _input(event: InputEvent) -> void:
+	if _currently_changing != "":
+		if event is InputEventJoypadButton or event is InputEventKey:
+			change_key(_currently_changing, event)
+			button_pressed(_currently_changing)
+		else:
+			push_warning("Unsupported InputEvent type.")
+
+
+func button_pressed(type: String) -> void:
+	if _currently_changing == "":
+		_currently_changing = type
+		change_text(_currently_changing, InputEvent.new(), true)
+	else:
+		var action_list := InputMap.get_action_list(_currently_changing)
+		change_text(_currently_changing, action_list[0])
+		_currently_changing = ""
+
+
+func change_key(button: String, event: InputEvent) -> void:
+	# load_keybinds function
+	InputMap.action_erase_events(str(button))
+	InputMap.action_add_event(str(button), event)
+
+	_currently_changing = ""
+	KEYBINDS[button] = event
+	change_text(button, event)
+
+
 # this script stinks. gonna become obsolete when i get actual good settings going w
 func change_offset(new_value: String) -> void:
 	global_offset = float(new_value) / 1000
@@ -69,6 +111,29 @@ func change_offset(new_value: String) -> void:
 func change_res(new_size: Vector2) -> void:
 	OS.window_size = new_size
 	_root.size = new_size
+
+
+func change_text(button: String, event: InputEvent, pressed := false) -> void:
+	var button_object: Button
+	match button:
+		"LeftDon":
+			button_object = _left_don_butt
+		"LeftKat":
+			button_object = _left_kat_butt
+		"RightDon":
+			button_object = _right_don_butt
+		"RightKat":
+			button_object = _right_kat_butt
+		_:
+			push_warning("Unknown input.")
+			return
+	if event is InputEventJoypadButton:
+		button_object.text = "Joystick Button %s" % (event as InputEventJoypadButton).button_index
+	elif event is InputEventKey:
+		button_object.text = OS.get_scancode_string((event as InputEventKey).scancode)
+	else:
+		button_object.text = "..."
+	button_object.pressed = pressed
 
 
 func hit_error(new_visible: bool) -> void:
@@ -88,8 +153,8 @@ func save_settings() -> void:
 	var config_file := ConfigFile.new()
 
 	print_debug("Saving keybinds config...")
-	for key in _keybind_changer.KEYBINDS:
-		config_file.set_value("Keybinds", str(key), _keybind_changer.KEYBINDS[key])
+	for key in KEYBINDS:
+		config_file.set_value("Keybinds", str(key), KEYBINDS[key])
 
 	print_debug("Saving resolution config...")
 	var res := _root.size
