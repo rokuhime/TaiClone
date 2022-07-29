a9eeac6f3515bfa5b1daef5dfe4da349db45122a
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-29 12:13:03 -0700

    Remove `global_tools.gd`

--- scripts/temploadchart.gd
+++ scripts/temploadchart.gd
@@ -13,9 +13,10 @@ func _process(_delta: float) -> void:
 
 func load_func() -> void:
 	_debug_text.text = "Loading... [Checking File]"
-	if File.new().file_exists(_file_input.text):
+	var file_path := _file_input.text.replace("\\", "/")
+	if File.new().file_exists(file_path):
 		_debug_text.text = "Loading... [Reading File]"
-		emit_signal("load_chart", _file_input.text)
+		emit_signal("load_chart", file_path)
 		_debug_text.text = "Done!"
 	else:
 		_debug_text.text = "Invalid file!"
