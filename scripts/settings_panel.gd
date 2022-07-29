b6755e5d10bfcf8107a33e75838cf31f0e338a5c
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-07-28 19:20:39 -0700

    Integrate `resolution_changer` into `settings_panel`

--- scripts/settings_panel.gd
+++ scripts/settings_panel.gd
@@ -4,7 +4,10 @@ signal hit_error_toggled(new_visible)
 signal late_early_changed(new_value)
 signal offset_changed
 
+onready var _root := $"/root" as Viewport
 onready var _v := $"ScrollContainer/VBoxContainer"
+
+onready var _dropdown := _v.get_node("Resolution/OptionButton") as OptionButton
 
 
 func _ready() -> void:
@@ -17,11 +20,33 @@ func _ready() -> void:
 	if settings.globalOffset != 0:
 		offset_text.text = str(settings.globalOffset)
 
+	_dropdown.add_item("16:9 | 1920x1080")
+	_dropdown.set_item_metadata(0, Vector2(1920, 1080))
+	_dropdown.add_item("16:9 | 1280x720")
+	_dropdown.set_item_metadata(1, Vector2(1280, 720))
+	_dropdown.add_item("16:9 | 1024x576")
+	_dropdown.set_item_metadata(2, Vector2(1024, 576))
+	_dropdown.add_separator()
+	_dropdown.add_item("4:3 | 1280x1024")
+	_dropdown.set_item_metadata(4, Vector2(1280, 1024))
+	_dropdown.add_item("4:3 | 1024x768")
+	_dropdown.set_item_metadata(5, Vector2(1024, 768))
+	_dropdown.add_separator()
+	_dropdown.add_item("5:4 | 1025x820")
+	_dropdown.set_item_metadata(7, Vector2(1025, 820))
+
+
+func change_res(index: int) -> void:
+	print_debug("Resolution changed to %s." % _dropdown.get_item_text(index))
+	var new_size := _dropdown.get_item_metadata(index) as Vector2
+	OS.window_size = new_size
+	_root.size = new_size
+
 
 # this script stinks. gonna become obsolete when i get actual good settings going w
 func change_offset(new_value: String) -> void:
 	settings.globalOffset = float(new_value) / 1000
-	print_debug("Offset set to: %s" % settings.globalOffset)
+	print_debug("Offset set to %s." % settings.globalOffset)
 	emit_signal("offset_changed")
 
 
@@ -35,6 +60,14 @@ func late_early(new_value: int) -> void:
 
 func save_settings() -> void:
 	settings.saveConfig()
+
+
+func toggle_fullscreen(new_visible: bool) -> void:
+	print_debug("Fullscreen set to %s." % new_visible)
+	OS.window_fullscreen = new_visible
+
+	for i in range(_dropdown.get_item_count()):
+		_dropdown.set_item_disabled(i, new_visible)
 
 
 func toggle_settings() -> void:
