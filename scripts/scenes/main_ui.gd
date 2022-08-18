485684e825ee3b3a2a5025ba3e849371a2225e19
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-08-17 20:36:59 -0700

    Merge branch 'FuzzyFus:2.0' into load-fus-files

---
+++ scripts/scenes/main_ui.gd
@@ -0,0 +1,62 @@
+extends Node
+
+onready var blackout = $Blackout as Control
+onready var blackoutTween = $Blackout/Tween as Tween
+
+onready var barTop = $Bars/Top as Control
+onready var barBottom = $Bars/Bottom as Control
+onready var barTween = $Bars/Tween as Tween
+
+onready var resultsObj = preload("res://game/scenes/results.tscn")
+onready var testObj = preload("res://game/scenes/test.tscn")
+
+var currentUI:String = "results"
+var optionsOpened:bool = false
+
+func changeUI(menuID):
+	print("changeUI from ", currentUI, " to ", menuID)
+	
+	#if just an options thing, toggle it
+	if menuID == "options":
+		optionsOpened = !optionsOpened
+
+	#if its a main ui object, and new isnt the same...
+	elif menuID != currentUI:
+		currentUI = menuID
+
+		#remove old ui
+		if $CurrentUI.get_child_count() != 0:
+			$CurrentUI.get_child(0).queue_free()
+
+		#get new ui
+		var newUI
+		match menuID:
+			"results":
+				newUI = resultsObj.instance()
+			"test":
+				newUI = testObj.instance()
+				newUI.get_node("AudioStreamPlayer").play(32.092)
+		$CurrentUI.add_child(newUI)
+
+func backButtonPressed():
+	toggleBlackout()
+	#figure out where we need to go
+	match(currentUI):
+		_:
+			print("back button pressed when unapplicable!")
+
+func toggleBlackout():
+	if not blackoutTween.remove(blackout, "self_modulate"):
+		push_warning("Attempted to remove blackout animation tween.")
+
+	#if blackout is invis...
+	if(blackout.modulate.a > 0):
+		if not blackoutTween.interpolate_property(blackout, "modulate", Color.white, Color.transparent, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT):
+			push_warning("Attempted to tween blackout animation.")
+	#if blackout is opaque...
+	else:
+		if not blackoutTween.interpolate_property(blackout, "modulate", Color.transparent, Color.white, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT):
+			push_warning("Attempted to tween blackout animation.")
+
+	if not blackoutTween.start():
+		push_warning("Attempted to start blackout animation tween.")
