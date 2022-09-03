832d7ffb07d39dbf7969323666747fd89bb86781
zachmanthethird <zachmanthethirdyt@gmail.com>
2022-09-02 20:01:45 -0700

    Create `ChartLoader` class and add logic

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -14,9 +14,6 @@ signal key_pressed(key)
 signal marker_added(type, timing, indicate)
 
 ## Comment
-enum NoteType {TIMING_POINT, BARLINE, DON, KAT, ROLL, SPINNER}
-
-## Comment
 var _auto := false
 
 ## Comment
@@ -30,9 +27,6 @@ var _current_combo := 0
 
 ## Comment
 var _f := File.new()
-
-## Comment
-var _fus := "user://debug.fus"
 
 ## Comment
 var _inactive := true
@@ -78,8 +72,8 @@ func _ready() -> void:
 	var _bars_removed := root_viewport.remove_scene("Bars")
 
 	# dev autoload map
-	if _f.file_exists(_fus):
-		load_func(_fus)
+	if _f.file_exists(ChartLoader.FUS):
+		load_func(ChartLoader.FUS)
 
 
 func _process(_delta: float) -> void:
@@ -279,189 +273,27 @@ func load_func(file_path := "") -> void:
 	if file_path == "":
 		file_path = line_edit.text.replace("\\", "/")
 
-	if _f.open(file_path, File.READ):
-		_load_finish("Invalid file!")
-		return
-
-	debug_text.text = "Loading... [Reading File]"
-
-	## Comment
-	var fus_version := "v0.0.3"
-
 	if file_path.ends_with(".osu"):
 		## Comment
-		var artist := ""
+		var load_error := ChartLoader.load_osu(file_path)
 
-		## Comment
-		var audio_filename := ""
-
-		## Comment
-		var bg_file_name := ""
-
-		## Comment
-		var charter := ""
-
-		## Comment
-		var cur_bpm := -1.0
-
-		## Comment
-		var current_meter := 0
-
-		## Comment
-		var current_timing_data := []
-
-		## Comment
-		var difficulty_name := ""
-
-		## Comment
-		var map_sv_multiplier := ""
-
-		## Comment
-		var next_barline := 0.0
-
-		## Comment
-		var next_timing := []
-
-		## Comment
-		var notes := []
-
-		## Comment
-		var section := ""
-
-		## Comment
-		var subsection := ""
-
-		## Comment
-		var title := ""
-
-		## Comment
-		var total_cur_sv := 0.0
-
-		while _f.get_position() < _f.get_len():
-			## Comment
-			var line := _f.get_line().strip_edges()
-
-			if line.empty():
-				continue
-
-			if line.begins_with("[") and line.ends_with("]"):
-				section = line.substr(1, line.length() - 2)
-				continue
-
-			## Comment
-			var line_data := line.split(",")
-
-			match section:
-				"Difficulty":
-					map_sv_multiplier = _find_value(map_sv_multiplier, line, "SliderMultiplier:")
-					total_cur_sv = float(map_sv_multiplier)
-
-				"Events":
-					if subsection == "Background and Video events":
-						bg_file_name = line.get_slice(",", 2).replace("\"", "")
-						subsection = ""
-
-					subsection = _find_value(subsection, line, "//")
-
-				"General":
-					audio_filename = _find_value(audio_filename, line, "AudioFilename:")
-
-				"HitObjects":
-					## Comment
-					var time := float(line_data[2]) / 1000
-
-					while not next_timing.empty():
-						## Comment
-						var meter := int(next_timing[1])
-
-						## Comment
-						var next_time := float(next_timing[0])
-
-						## Comment
-						var timing := float(next_timing[2])
-
-						if cur_bpm == -1 and meter:
-							while next_time > min(-1, time - 1):
-								next_time -= 60 * meter / timing
-
-							next_barline = next_time
-
-						if next_time > time:
-							break
-
-						if meter:
-							while next_barline < next_time:
-								next_barline = _barline(total_cur_sv, notes, next_barline, current_meter, cur_bpm)
-
-							cur_bpm = timing
-							current_meter = meter
-							next_barline = next_time
-							_append_note(notes, [next_barline, cur_bpm, NoteType.TIMING_POINT])
-
-						else:
-							total_cur_sv = timing * float(map_sv_multiplier)
-
-						if current_timing_data.empty():
-							next_timing = []
-
-						else:
-							next_timing = str(current_timing_data.pop_front()).split(",")
-
-					while next_barline <= time:
-						next_barline = _barline(total_cur_sv, notes, next_barline, current_meter, cur_bpm)
-
-					if 1 << 3 & int(line_data[3]):
-						_append_note(notes, [time, total_cur_sv, NoteType.SPINNER, float(line_data[5]) / 1000 - time])
-						continue
-
-					## Comment
-					var finisher_int := 1 << 2 & int(line_data[4])
-
-					if 1 << 1 & int(line_data[3]):
-						_append_note(notes, [time, total_cur_sv, NoteType.ROLL, float(line_data[7]) * int(line_data[6]) * 0.6 / cur_bpm / total_cur_sv, finisher_int])
-
-					else:
-						_append_note(notes, [time, total_cur_sv, NoteType.KAT if bool(((1 << 1) + (1 << 3)) & int(line_data[4])) else NoteType.DON, finisher_int])
-
-				"Metadata":
-					artist = _find_value(artist, line, "Artist:")
-					charter = _find_value(charter, line, "Creator:")
-					difficulty_name = _find_value(difficulty_name, line, "Version:")
-					title = _find_value(title, line, "Title:")
-
-				"TimingPoints":
-					## Comment
-					var uninherited := bool(int(line_data[6]))
-
-					line_data = _csv_line([float(line_data[0]) / 1000, int(line_data[2]) if uninherited else 0, (60000 if uninherited else -100) / float(line_data[1])])
-					if next_timing.empty():
-						next_timing = line_data
-
-					else:
-						current_timing_data.append(line_data.join(","))
-
-		_f.close()
-
-		## Comment
-		var folder_path := file_path.get_base_dir()
-
-		file_path = _fus
-		if _f.open(file_path, File.WRITE):
-			_load_finish("Unable to create temporary .fus file!")
+		if load_error:
+			_load_finish(load_error)
 			return
 
-		_f.store_string(_csv_line([fus_version, folder_path.plus_file(bg_file_name), folder_path.plus_file(audio_filename), artist, charter, difficulty_name, title] + notes).join("\n"))
-		_f.close()
+		file_path = ChartLoader.FUS
 
 	if not file_path.ends_with(".fus"):
 		_load_finish("Invalid file!")
 		return
 
+	debug_text.text = "Loading... [Reading File]"
+
 	if _f.open(file_path, File.READ):
 		_load_finish("Unable to read temporary .fus file!")
 		return
 
-	if _f.get_line() != fus_version:
+	if _f.get_line() != ChartLoader.FUS_VERSION:
 		_load_finish("Outdated .fus file!")
 		return
 
@@ -504,29 +336,29 @@ func load_func(file_path := "") -> void:
 		var total_cur_sv := float(line_data[1]) * cur_bpm * 5.7
 
 		match int(line_data[2]):
-			NoteType.BARLINE:
+			ChartLoader.NoteType.BARLINE:
 				## Comment
 				var hit_object := root_viewport.bar_line_object.instance() as BarLine
 
 				hit_object.change_properties(timing, total_cur_sv)
 				add_object(hit_object)
 
-			NoteType.DON, NoteType.KAT:
+			ChartLoader.NoteType.DON, ChartLoader.NoteType.KAT:
 				## Comment
 				var hit_object := root_viewport.note_object.instance() as Note
 
-				hit_object.change_properties(timing, total_cur_sv, int(line_data[2]) == int(NoteType.KAT), bool(int(line_data[3])))
+				hit_object.change_properties(timing, total_cur_sv, int(line_data[2]) == int(ChartLoader.NoteType.KAT), bool(int(line_data[3])))
 				add_object(hit_object)
 				Root.send_signal(self, "new_marker_added", hit_object, "add_marker")
 
-			NoteType.ROLL:
+			ChartLoader.NoteType.ROLL:
 				## Comment
 				var hit_object := root_viewport.roll_object.instance() as Roll
 
 				hit_object.change_properties(timing, total_cur_sv, float(line_data[3]), bool(int(line_data[4])), cur_bpm)
 				add_object(hit_object)
 
-			NoteType.SPINNER:
+			ChartLoader.NoteType.SPINNER:
 				## Comment
 				var hit_object := root_viewport.spinner_warn_object.instance() as SpinnerWarn
 
@@ -534,7 +366,7 @@ func load_func(file_path := "") -> void:
 				add_object(hit_object)
 				Root.send_signal(self, "object_added", hit_object, "add_object")
 
-			NoteType.TIMING_POINT:
+			ChartLoader.NoteType.TIMING_POINT:
 				cur_bpm = float(line_data[1])
 
 	get_tree().call_group("HitObjects", "apply_skin", root_viewport.skin)
@@ -560,33 +392,6 @@ func play_button_pressed() -> void:
 func toggle_settings() -> void:
 	if not root_viewport.remove_scene("SettingsPanel"):
 		root_viewport.add_scene(root_viewport.settings_panel.instance(), name)
-
-
-## Comment
-static func _append_note(notes: Array, line_data: Array) -> void:
-	notes.append(_csv_line(line_data).join(","))
-
-
-## Comment
-static func _barline(total_cur_sv: float, notes: Array, next_barline: float, current_meter: int, cur_bpm: float) -> float:
-	_append_note(notes, [next_barline, total_cur_sv, NoteType.BARLINE])
-	return next_barline + 60 * current_meter / cur_bpm
-
-
-## Comment
-static func _csv_line(line_data: Array) -> PoolStringArray:
-	## Comment
-	var csv_line := []
-
-	for key in line_data:
-		csv_line.append(str(key))
-
-	return PoolStringArray(csv_line)
-
-
-## Comment
-static func _find_value(value: String, line: String, key: String) -> String:
-	return line.trim_prefix(key).strip_edges() if line.begins_with(key) else value
 
 
 ## Comment
