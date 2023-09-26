d66d7e6b1e70d865afc671a0414e3678f93f582f
FuzzyFus <creeperking14@gmail.com>
2023-09-26 05:13:24 -0700

    added finisher sounds to skin manager

--- scripts/skin_manager.gd
+++ scripts/skin_manager.gd
@@ -5,6 +5,8 @@ var colour_kat := Color("438EAD")
 
 @onready var audio_don := load("res://assets/default_skin/h_don.wav")
 @onready var audio_kat := load("res://assets/default_skin/h_kat.wav")
+@onready var audio_don_f := load("res://assets/default_skin/hf_don.wav")
+@onready var audio_kat_f := load("res://assets/default_skin/hf_kat.wav")
 
 @onready var hitin_accurate := load("res://assets/default_skin/taiko-hit300k.png")
 @onready var hitin_inaccurate := load("res://assets/default_skin/taiko-hit100k.png")
