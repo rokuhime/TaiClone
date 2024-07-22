90084da118da26c62ee5adf9372695016405c1ef
FuzzyFus <creeperking14@gmail.com>
2024-07-22 11:00:52 -0700

    basic songselectv2 movement

--- scenes/song_select/songselectv2.gd
+++ scenes/song_select/songselectv2.gd
@@ -4,9 +4,13 @@ var music: AudioStreamPlayer
 var chart_listing_scene := preload("res://entites/songselect/chart_listing.tscn")
 
 @onready var listing_container := $ListingContainer
-var list_movement_tween: Tween
+var listing_container_tween: Tween
 
 @export var selected_listing_idx := 0
+
+const LISTING_SEPARATION := 10.0
+const TUCK_AMOUNT := 150.0
+const LISTING_MOVEMENT_TIME := 0.5
 
 func populate_from_folder(folder_path: String) -> void:
 	if not DirAccess.dir_exists_absolute(folder_path):
@@ -32,6 +36,7 @@ func populate_from_folder(folder_path: String) -> void:
 		continue
 	
 	Global.push_console("SongSelectV2", "finished!", 0)
+	update_visual()
 
 func create_listing(chart: Chart) -> ChartListing:
 	var listing := chart_listing_scene.instantiate() as ChartListing
@@ -50,7 +55,47 @@ func listing_exists(chart: Chart) -> bool:
 func update_visual() -> void:
 	var i := 0
 	var listing_size: Vector2
+	
 	for listing in listing_container.get_children():
-		if listing_size == null:
+		if not listing_size:
 			listing_size = listing.size
-		listing.position = Vector2(0, listing_size.y * i - selected_listing_idx)
+		
+		listing.position.y = (listing_size.y  + LISTING_SEPARATION) * i
+		var listing_position := -listing_size.x if i == selected_listing_idx else -listing_size.x + TUCK_AMOUNT
+		
+		if listing.movement_tween:
+			listing.movement_tween.kill()
+		listing.movement_tween = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+		listing.movement_tween.tween_property(listing, "position:x", listing_position, LISTING_MOVEMENT_TIME)
+		
+		i += 1
+	
+	# move listing container to center selected chart
+	if listing_container_tween:
+		listing_container_tween.kill()
+	listing_container_tween = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	
+	var selected_listing_location = (listing_size.y + LISTING_SEPARATION) * -selected_listing_idx
+	print(selected_listing_location)
+	listing_container_tween.tween_property(listing_container, "position:y", selected_listing_location, LISTING_MOVEMENT_TIME)
+
+func _unhandled_input(event):
+	# refresh listings
+	#if event is InputEventKey and event.keycode == KEY_F5:
+		#refresh_listings_from_song_folders()
+	
+	if listing_container.get_child_count() > 0:
+		if event.is_action_pressed("LeftKat"):
+			change_selected_listing(-1)
+		elif event.is_action_pressed("RightKat"):
+			change_selected_listing(1)
+		elif event.is_action_pressed("LeftDon") or event.is_action_pressed("LeftDon") or event.is_action_pressed("ui_accept"):
+			#transition_to_gameplay()
+			Global.push_console("SongSelectV2", "transition_to_gameplay()")
+
+func change_selected_listing(idx: int, exact := false):
+	if exact:
+		selected_listing_idx = idx % listing_container.get_child_count()
+	else:
+		selected_listing_idx = wrap((selected_listing_idx + idx) % listing_container.get_child_count(), 0, listing_container.get_child_count())
+	update_visual()
